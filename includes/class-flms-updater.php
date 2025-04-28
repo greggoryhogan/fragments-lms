35ec7b56ba19de37754ad0d669f6a257271d13af
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-04-28 15:19:20 -0400

    1.1.8

--- includes/class-flms-updater.php
+++ includes/class-flms-updater.php
@@ -6,19 +6,16 @@
  * @version 1.0.0
  */
 
-defined( 'ABSPATH' ) || exit;
 
-//Plugin updater
-//if (is_admin()) {
-	
-	include_once dirname( FLMS_PLUGIN_FILE ) . '/assets/library/plugin-update-checker-5.5/plugin-update-checker.php';
-	
-	use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://github.com/greggoryhogan/fragments-lms/', // GitHub URL of your repo
-		__FILE__, // Path to your plugin file
-		'fragments-lms' // The plugin slug (unique identifier for your plugin)
-	);
-	$myUpdateChecker->setBranch('master');
 
-//}
+require dirname( FLMS_PLUGIN_FILE ) . '/assets/library/plugin-update-checker-5.5/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/greggoryhogan/fragments-lms/', // GitHub URL of your repo
+	FLMS_PLUGIN_FILE, // Path to your plugin file
+	'fragments-lms' // The plugin slug (unique identifier for your plugin)
+);
+
+$myUpdateChecker->setBranch('master');
