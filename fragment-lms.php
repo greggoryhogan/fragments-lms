ea3a07c5c86bfc576b1bdd413b12c7ee4d591b18
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-07-28 15:08:39 -0400

    Version plugin

--- fragment-lms.php
+++ fragment-lms.php
@@ -3,7 +3,7 @@
 Plugin Name:  Fragments LMS
 Plugin URI:	  https://fragmentlms.com
 Description:  Learning management software built for developers, by developers
-Version:	  1.1.30
+Version:	  1.1.31
 Author:		  Fragment
 Author URI:   https://fragmentwebworks.com
 License:      GPL2
